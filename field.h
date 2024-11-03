75edc3651bb58c0a9101317034cc5cabc1bf4085
Jack Martin <jhmartin4510@vesta.unm.edu>
2024-11-03 09:06:39 -0700

    This is the first iteration with working functions. Looking at adding additional comments and optimization

---
+++ field.h
@@ -0,0 +1,143 @@
+#include<iostream>
+#include<cmath>
+
+#ifndef _FIELD_H_
+#define _FIELD_H_
+
+class Electric_field
+{
+	private:
+		double *E;
+	public:
+//function
+	
+	Electric_field()
+	{
+		E = new double[3];
+		std::cout<<"default constructor called" <<std::endl;
+	}
+	Electric_field(double x, double y, double z)
+	{
+		E = new double[3];
+		*(E+0) = x;
+		*(E+1) = y;
+		*(E+2) = z;
+		std::cout<<"Other constructor called" <<std::endl;
+	}
+//setters
+	void setx(double x){
+		*(E+0) = x;
+	} 
+	void sety(double y){
+		*(E+1) = y;
+	}
+	void setz(double z){
+		*(E+2) = z;
+	}
+//getters
+	double getx()
+	{
+		return *(E+0);
+	}
+	double gety()
+	{
+		return *(E+1);
+	}
+	double getz()
+	{
+		return *(E+2);
+	}
+//functions
+	double calculateMagnitude(){
+		double x2 = (*(E+0))*(*(E+0));
+		double y2 = (*(E+1))*(*(E+1));
+		double z2 = (*(E+2))*(*(E+2));
+		return sqrt(x2+y2+z2); 
+	}
+	double inner_product(){
+		return calculateMagnitude()*calculateMagnitude();	
+	}
+//destructor
+	~Electric_field(){
+		delete[] E;
+		std::cout<<"Destructor Call complete" <<std::endl;
+	}
+};
+
+class Magnetic_field
+{
+	private: 
+		double *M;
+	public:
+//constructors 
+	Magnetic_field()
+	{
+		M = new double[3];	
+	}
+	
+	Magnetic_field(double x, double y, double z)
+	{
+		M = new double[3];
+		*(M+0) = x;
+		*(M+1) = y;		
+		*(M+2) = z;
+		std::cout<<"Parameter constructor called" <<std::endl;
+	}
+//setters
+	void setx(double x)
+	{
+		*(M+0) = x;
+	}
+	void sety(double y)
+	{
+		*(M+1) = y;
+	}
+	void setz(double z)
+	{
+		*(M+2) = z;
+	}
+//getters
+	double getx()
+	{
+		return *(M+0);
+	}
+	double gety()
+	{
+		return *(M+1);
+	}
+	double getz()
+	{
+		return *(M+2);
+	}
+//functions
+	double calculateMagnitude()
+	{
+		int sum =0;
+		for(int i=0; i<2; i++)
+		{
+			sum += (*(M+i))*(*(M+i));
+		}
+		return sqrt(sum);
+	}
+	double unit_vx()
+	{
+		return (*(M+0))/calculateMagnitude();		
+	}
+	double unit_vy()
+	{
+		return (*(M+1))/calculateMagnitude();
+	}
+	double unit_vz()
+	{
+		return (*(M+2))/calculateMagnitude();
+	}
+
+//destructor
+	~Magnetic_field()
+	{
+	delete[] M;
+	std::cout<<"Default destructor called" <<std::endl;	
+	}
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include "field.h"
+#include <iostream>
+
+
+int main()
+{
+//Default Constructer
+Electric_field E1;
+
+//Parameter Constructer
+Electric_field E2(3,4,6);
+
+//Using the getter functions of Electric Field Constructer
+std::cout<<"Printing values of E2" <<std::endl;
+std::cout<<E2.getx() <<"\t" <<E2.gety() <<"\t" <<E2.getz() <<std::endl;
+//Calculating and printing the Magnitude of the vector
+std::cout<<"The magnitude of E2 is: " <<E2.calculateMagnitude() <<std::endl;
+
+
+//using set functions of Electric field
+E1.setx(3);
+E1.sety(4);
+E1.setz(0);
+//using the functions with the object created by the Default Constructor
+std::cout<<"Printing the values of E1" <<std::endl;
+std::cout<<E1.getx() <<"\t" <<E1.gety() <<"\t" <<E1.getz() <<std::endl;
+std::cout<<"The magnitude of E1 is: " <<E1.calculateMagnitude() <<std::endl;
+//calculating the inner product and printing the value
+std::cout<<"Inner prodcut of E1 is: " <<E1.inner_product() <<std::endl;
+
+
+//Parameter Constructor
+Magnetic_field M1(3,4,6);
+//Calcuating the unit vector components and printing them
+std::cout<<"Unit Vector is: (" <<M1.unit_vx() <<"," <<M1.unit_vy() <<"," <<M1.unit_vz() <<")" <<std::endl;
+	
+return 0;
+}
